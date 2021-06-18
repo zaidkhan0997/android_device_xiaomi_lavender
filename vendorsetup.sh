459a0d1ec49b3cb41811522b89417d56d2141058
zaidkhan0997 <kzaid0997@gmail.com>
2021-06-18 19:36:04 +0530

    lavender: add vendorsetup.sh
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+# clone
+git clone -b 11 https://github.com/zaidkhan0997/vender_xiaomi_GoogleCameraMod.git vendor/xiaomi/GoogleCameraMod
+git clone -b 11 https://github.com/zaidkhan0997/hardware_qcom-caf_display_msm8998.git hardware/qcom-caf/sdm660-R/display
+git clone -b 11 https://github.com/zaidkhan0997/hardware_qcom-caf_media_msm8998.git hardware/qcom-caf/sdm660-R/media
+git clone -b 11 https://github.com/zaidkhan0997/hardware_qcom-caf_audio_msm8998.git hardware/qcom-caf/sdm660-R/audio
