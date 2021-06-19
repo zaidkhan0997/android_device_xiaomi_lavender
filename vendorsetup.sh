ce9f741ce2be890f0b55706051e0f1ba1b997387
zaidkhan0997 <kzaid0997@gmail.com>
2021-06-20 00:04:18 +0530

    lavender: use custom R hals
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,10 @@
+# nuke
+rm -rf hardware/qcom-caf/msm8998/display
+rm -rf hardware/qcom-caf/msm8998/media
+rm -rf hardware/qcom-caf/msm8998/audio
+
 # clone
 git clone -b 11 https://github.com/zaidkhan0997/vender_xiaomi_GoogleCameraMod.git vendor/xiaomi/GoogleCameraMod
+git clone -b 11 https://github.com/zaidkhan0997/hardware_qcom-caf_display_msm8998.git hardware/qcom-caf/msm8998/display
+git clone -b 11 https://github.com/zaidkhan0997/hardware_qcom-caf_media_msm8998.git hardware/qcom-caf/msm8998/media
+git clone -b 11 https://github.com/zaidkhan0997/hardware_qcom-caf_audio_msm8998.git hardware/qcom-caf/msm8998/audio
