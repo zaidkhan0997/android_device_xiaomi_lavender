bc8d5acb3db9d81e4f44d5b4ebd3d7e765a8791e
MUHAMAD KHOIRON <47946725+Doraemon-Chan@users.noreply.github.com>
2021-08-07 17:03:19 +0700

    Update kangos_lavender.mk

--- kangos_lavender.mk
+++ kangos_lavender.mk
@@ -66,10 +66,10 @@ PRODUCT_MODEL := Redmi Note 7
 PRODUCT_BUILD_PROP_OVERRIDES += \
         TARGET_DEVICE="lavender" \
         PRODUCT_NAME="lavender" \
-        PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210605.005 7349499 release-keys"
+        PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
 
 # Build Fingerprint
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
 
 TARGET_VENDOR_PRODUCT_NAME := lavender
 
